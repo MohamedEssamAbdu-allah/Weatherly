6f56e6438a764c30f0ed29649cbfbbec93554e4d
MohamedEssam <121412932+MohamedEssamAbdu-allah@users.noreply.github.com>
2023-03-25 00:27:42 +0200

    added home screen and week screen that shows weather details for 7 days

--- app/src/main/java/com/example/weatherly/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherly/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.example.weatherly.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -24,14 +25,22 @@ class HomeFragment : Fragment() {
     ): View {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
+        /*
+        NOTE UNICODES FOR WEATHER PREFERENCES
+        Celsius = \u2103
+        Fahrenheit = \u2109
+         */
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.weekTv.setOnClickListener {
+            Navigation.findNavController(root)
+                .navigate(HomeFragmentDirections.actionNavHomeToWeekFragment())
         }
+        //val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
---
+++ app/src/main/java/com/example/weatherly/ui/week/WeekViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.weatherly.ui.week
+
+import androidx.lifecycle.ViewModel
+
+class WeekViewModel : ViewModel() {
+}
