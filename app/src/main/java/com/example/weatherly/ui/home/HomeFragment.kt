e7b8945ec30444ebdac0a97ad0bc63b6da1245e9
MohamedEssam <121412932+MohamedEssamAbdu-allah@users.noreply.github.com>
2023-03-25 03:56:49 +0200

    added time and date to home screen

--- app/src/main/java/com/example/weatherly/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherly/ui/home/HomeFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.weatherly.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -37,9 +37,17 @@ class HomeFragment : Fragment() {
             Navigation.findNavController(root)
                 .navigate(HomeFragmentDirections.actionNavHomeToWeekFragment())
         }
+
+
+        homeViewModel.time.observe(viewLifecycleOwner) {
+            binding.timeTv.text = it
+        }
+        homeViewModel.date.observe(viewLifecycleOwner){
+            binding.dateTv.text = it
+        }
         //val textView: TextView = binding.textHome
 //        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
+//            binding.timeTv.text = it
 //        }
         return root
     }
